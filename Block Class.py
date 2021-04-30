45c3fc801dcb882caa0c6bf6ce2caa823d3de1ab
kpeabody <83436808+kpeabody@users.noreply.github.com>
2021-04-29 22:12:28 -0600

    Block Class

---
+++ Block Class.py
@@ -0,0 +1,10 @@
+class Block:
+    def __init__(self, x, y, width, height, IDGround, IDAir):
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+        self.IDGround = IDGround
+        self.IDAir = IDAir
+
+block = Block(1,1,100,100,0,1)
