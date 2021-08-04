36bc6bf7969d284941bf03d332c7b0edfa23d45b
kpeabody <83436808+kpeabody@users.noreply.github.com>
2021-08-04 02:58:54 -0600

    Update and rename Game11.py to Game12.py
    Game 12 commit.

---
+++ Game12.py
@@ -0,0 +1,23 @@
+import sys
+import turtle
+import numpy as np
+from WindowClass1 import Window1
+from BlockClass1 import Block
+
+#I only need a turtle object when I am wanting to draw a turtle
+#Bg stands for background image
+
+#Used to speed up the speed of complex graphics.  It will basically draw on the screen n number of times.
+screen = turtle.Screen()
+
+GameWindow = Window1(800, 600, 20, 7, screen)
+
+row_count = 7
+column_count = 7
+count = 0
+
+block = Block(-300,-100,20,0)
+block.DrawGrid(row_count, column_count)
+
+#This keeps the turtle objects drawing on the screen
+turtle.done()
