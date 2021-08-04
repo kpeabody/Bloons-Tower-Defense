5a69765a74cfdcde0b9c771f17c46c8ae0d0f11d
kpeabody <83436808+kpeabody@users.noreply.github.com>
2021-08-04 02:57:54 -0600

    Update and rename Game10.py to Game11.py
    Game 11 commit.

--- Game11.py
+++ Game11.py
@@ -1,5 +1,5 @@
 import turtle
-import array
+import numpy as np
 from WindowClass1 import Window1
 from BlockClass1 import Block
 
@@ -11,10 +11,6 @@ screen = turtle.Screen()
 
 GameWindow = Window1(800, 600, 20, 7, screen)
 
-i = 5
-j = 5
-blockArray1 = []
-blockArray2 = []
 '''
 for col in range(len(blockArray1)):
     blockArray1.append([Block(-300,-100,20,0)])
@@ -40,15 +36,19 @@ for i in range(i):
     blockArray2.append(Block(-300 + ((count+i)*20),-100,20,0))
     blockArray2[i+count+1].DrawBlock()
 '''
+r = 1
+c = 1
 count = 0
-for i in range(i):
-    for j in range(j):
-        blockArray2.append(Block(-300 + (i*20),-100 + (j*20),20,0))
-        blockArray2.append(Block(-300 + (j*20),-100 + (i*20),20,0))
-
-for count in range(i+j):
-    blockArray2[count].DrawBlock()
+blockArray1 = np.array(Block(-300 + 0*20,-100,20,0))
+blockArray2 = np.array([Block(-300 + 1*20,-100,20,0)])
+blockArray1 = np.expand_dims(blockArray2, axis=0)
+for c in range(c):
+    for r in range(r):
+        blockArray1[r][c].DrawBlock()
+        print("Custom: (" + str(r) + "," + str(c) + ")")
+print(blockArray1.shape)
+print(blockArray1.ndim)
+print(blockArray1)
 
 #This keeps the turtle objects drawing on the screen
 turtle.done()
-
