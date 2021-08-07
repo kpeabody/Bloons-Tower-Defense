1e42a4be7073da0f3bed1e501ea3a49776298b5a
kpeabody <83436808+kpeabody@users.noreply.github.com>
2021-08-07 01:51:17 -0600

    Update Game12.py
    Commit goes along with various other code I have changed.  This includes the very beginning of the CreateArray function within my BlockClass.

--- Game12.py
+++ Game12.py
@@ -1,5 +1,3 @@
-import sys
-import turtle
 import numpy as np
 from WindowClass1 import Window1
 from BlockClass1 import Block
@@ -12,12 +10,13 @@ screen = turtle.Screen()
 
 GameWindow = Window1(800, 600, 20, 7, screen)
 
-row_count = 7
-column_count = 7
+row_count = 20
+column_count = 30
 count = 0
 
 block = Block(-300,-100,20,0)
 block.DrawGrid(row_count, column_count)
+block.CreateArray()
 
 #This keeps the turtle objects drawing on the screen
 turtle.done()
