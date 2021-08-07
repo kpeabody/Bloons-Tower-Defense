788ad21bb6202fe41ec11ebd838f34355338ef38
kpeabody <83436808+kpeabody@users.noreply.github.com>
2021-08-07 06:56:10 -0600

    Update Game12.py
    Final working code before using PyGame instead of Turtle.

--- Game12.py
+++ Game12.py
@@ -1,3 +1,5 @@
+import sys
+import turtle
 import numpy as np
 from WindowClass1 import Window1
 from BlockClass1 import Block
@@ -10,13 +12,13 @@ screen = turtle.Screen()
 
 GameWindow = Window1(800, 600, 20, 7, screen)
 
-row_count = 20
-column_count = 30
+row_count = 21
+column_count = 31
 count = 0
 
-block = Block(-300,-100,20,0)
+block = Block(-300,280,20,0)
+block.PrintArray()
 block.DrawGrid(row_count, column_count)
-block.CreateArray()
 
 #This keeps the turtle objects drawing on the screen
 turtle.done()
