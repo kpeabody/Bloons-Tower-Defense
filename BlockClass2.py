171e52853588c958656ddd48d26809e57a9554ab
kpeabody <83436808+kpeabody@users.noreply.github.com>
2021-08-08 01:07:05 -0600

    Update and rename BlockClass1.py to BlockClass2.py
    BlockClass1 renamed to BlockClass2.  This is has the code from the very first time I used PyGame in this class.

--- BlockClass2.py
+++ BlockClass2.py
@@ -1,18 +1,20 @@
-import turtle
+import pygame as py
 import numpy as np
 #Should I make the block class extend the turtle class?
 #This way the Block class will have the same properties as turtle with some additional properties like x, y, width, height, IDGround, IDAir
 #I think I need to have the block class inherit the turtle class 100%
 class Block():
-    blockturtle = turtle.Turtle()
     testarray = np.genfromtxt('map1.txt', delimiter = ',')
-    turtle.addshape('grass.gif')
-    turtle.addshape('redgrass.gif')
+    grass = py.image.load('grass.gif')
+    redgrass = py.image.load('redgrass.gif')
     def __init__(self, x, y, size, Id):
         self.x = x
         self.y = y
         self.size = size
         self.Id = Id
+    width = 800
+    height = 600
+    blockpy = py.display.set_mode((width, height))
 
     def GetXCoor(self):
         return self.x
@@ -33,15 +35,12 @@ class Block():
         self.size = size
 
     def DrawBlock(self):
-        self.blockturtle.penup()
-        self.blockturtle.speed(0)
         if(self.Id == 0):
-            self.blockturtle.shape('grass.gif')
+            self.blockpy.blit(grass, (self.x, self.y))
         elif(self.Id == 1):
-            self.blockturtle.shape('redgrass.gif')
+            self.blockpy.blit(redgrass, (self.x,self.y))
         #Creates Duplicates of the blockturtle object.  THis way we aren't changing data for the same turtle each time.
-        self.blockturtle.stamp()
-
+    
     def DrawGrid(self, row_count, column_count):
         #x constant which is set equal to the initial x position
         xCons = self.x
@@ -49,27 +48,10 @@ class Block():
         yCons = self.y
         for c in range(column_count):
             for r in range(row_count):
-                self.blockturtle.penup()
                 self.SetXCoor(xCons + (c*self.size))
                 self.SetYCoor(yCons - (r*self.size))
                 if(self.testarray[r][c]==1):
                     self.Id = 1
                 elif(self.testarray[r][c]==0):
                     self.Id = 0
-                '''
-                if(self.y <= (yCons + 0*self.size)):
-                    self.Id = 1
-                elif(self.x == xCons + (column_count-1)*self.size and self.y <= yCons + int(row_count/5)*self.size):
-                    self.Id = 1
-                elif(self.y == yCons + int(row_count/5)*self.size):
-                    self.Id = 1
-                elif(self.x == xCons + 0*self.size and self.y >= yCons + int(row_count/5)*self.size and self.y <= yCons + int(2*row_count/5)*self.size):
-                    self.Id = 1
-                else:
-                    self.Id = 0
-                '''
-                self.blockturtle.setpos(self.x,self.y)
                 self.DrawBlock()
-
-    def PrintArray(self):
-        print(self.testarray)
