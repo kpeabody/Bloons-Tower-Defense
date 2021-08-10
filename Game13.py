b1960dd7f1ed90a433b32ace307a9642f62a31b1
kpeabody <83436808+kpeabody@users.noreply.github.com>
2021-08-09 21:45:22 -0600

    Update Game13.py
    My path is working on my grid.  I think I am going to combine my BlockClass and Game class next.

--- Game13.py
+++ Game13.py
@@ -1,7 +1,6 @@
 import sys
 import numpy as np
 import pygame as py
-from WindowClass2 import Window1
 from BlockClass2 import Block
 
 width = 800
@@ -13,6 +12,9 @@ py.init()
 
 screen = py.display.set_mode((width, height))
 
+grass = py.image.load('grass.gif')
+redgrass = py.image.load('redgrass.gif')
+
 while not gameover:
     for event in py.event.get():
         if event.type == py.QUIT:
@@ -21,10 +23,17 @@ while not gameover:
         column_count = 31
         count = 0
 
-        block = Block(-300,280,20,0)
-        block.PrintArray()
-        block.DrawGrid(row_count, column_count)
-        
-        py.display.update() 
+        block = Block(-300,280,20,0,screen)
+        block.CreateGrid(row_count, column_count)
 
-#GameWindow = Window1(800, 600, 20, 7, screen
+        testarray2 = block.GetTestArray()
+        print(testarray2)
+
+        for r in range(row_count):
+            for c in range(column_count):
+                if(testarray2[r][c] == 0):
+                    screen.blit(grass, (c*block.GetSize(), r*block.GetSize()))
+                elif(testarray2[r][c] == 1):
+                    screen.blit(redgrass, (c*block.GetSize(), r*block.GetSize()))
+        
+        py.display.update()
